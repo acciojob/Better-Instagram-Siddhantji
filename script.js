22f08f1d10485dd2105bc61122dd11b1acd8df49
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-03 14:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const divs = document.querySelectorAll(".image");
 
-    let draggedItem = null;
-
-    divs.forEach(div => {
-        div.addEventListener("dragstart", function () {
-            draggedItem = div;
-        });
-
-        div.addEventListener("dragend", function () {
-            draggedItem = null;
-        });
-
-        div.addEventListener("dragover", function (event) {
-            event.preventDefault();
-        });
-
-        div.addEventListener("drop", function (event) {
-            event.preventDefault();
-            if (draggedItem !== null && draggedItem !== div) {
-                const temp = div.style.backgroundImage;
-                div.style.backgroundImage = draggedItem.style.backgroundImage;
-                draggedItem.style.backgroundImage = temp;
-            }
-        });
-    });
-});
+window.onload = function() {
+  let divs = document.querySelectorAll('.image');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].setAttribute('id', 'div' + (i + 1));
+    divs[i].addEventListener("dragstart", handleDragStart, false);
+    divs[i].addEventListener("dragover", handleDragOver, false);
+    divs[i].addEventListener("drop", handleDrop, false);
+  }
+}
+ 
+let dragSrcEl = null;
+ 
+function handleDragStart(e) {
+  
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+}
+ 
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+ 
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl != this) {
+    let parent = document.querySelector('#parent');
+    let srcIndex = Array.prototype.indexOf.call(parent.children, dragSrcEl);
+    let targetIndex = Array.prototype.indexOf.call(parent.children, this);
+    if (srcIndex < targetIndex) {
+      parent.insertBefore(dragSrcEl, this.nextSibling);
+      parent.insertBefore(this, parent.children[srcIndex]);
+    } else {
+      parent.insertBefore(this, dragSrcEl);
+      parent.insertBefore(dragSrcEl, parent.children[targetIndex]);
+    }
+  }
+  return false;
+}
